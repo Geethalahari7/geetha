774f9ef9688a4dcc355d3929ff71151014417db2
Geethalahari7 <160226381+Geethalahari7@users.noreply.github.com>
2024-10-22 10:09:30 +0530

    Create elif (example program).py

---
+++ elif (example program).py
@@ -0,0 +1,13 @@
+m=int(input())
+if m>=90:
+	print('Grade A')
+elif m>=80:
+	print('Grade B')
+elif m>=70:
+	print('Grade C')
+elif m>=60:
+	print('Grdae D')
+elif m>=50:
+	print('Grade E')
+else:
+	print('FAIL')
